fe16f46eeb77f194b6b61b39e5ac16f2eeb626ba
kyyamagu <kyyamagu@r.recruit.co.jp>
2017-02-07 15:54:01 +0900

    Clear test data

--- Tests/DeviceTests.swift
+++ Tests/DeviceTests.swift
@@ -18,6 +18,11 @@ final class DeviceTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        Device.Cached.deviceIdentifier = ""
+        Device.Cached.systemName       = ""
+        Device.Cached.systemVersion    = ""
+        Device.Cached.deviceModel      = ""
     }
     
     // MARK: deviceIdentifier
